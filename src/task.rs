808e9559001f36456f99b47c08b43e68d9c0859c
David Huss <dh@widerstandzwecklos.at>
2018-09-26 18:02:58 +0200

    Updated Documentation

--- src/task.rs
+++ src/task.rs
@@ -2,28 +2,53 @@ use ::*;
 
 
 
-/// A Task must fulfill these requirements:
-/// - serialization/deserialization via serde
-/// - keep track of time: start, end
-/// - keep track of status: waiting, running, finished, errored, aborted, paused
-/// - allow to construct commands on the fly
 
-
+/// A Task is what gets passed from watchdog to qu and from qu to worker. Tasks
+/// are essentially atomic units of work, broken down so they can be fairly managed
+/// by the queue.
+///
+/// This is basically a wrapper around a command, that allows us to keep track of
+/// a Tasks status, its start and end times etc. It consists of:
+/// - a _Status_ which manages the States of a Task and the allowed transitions between it \
+/// (e.g. a finished task cannot be aborted, a errored task cannot start etc.)
+/// - a _JobTime_ which allows to keep track of _when_ a certain state change has occured, as \
+/// well as the calculation of durations (the same construct is used for Job)
+/// - a _Command_ which allows to abstract CLI commands to be executed on the worker machines \
+/// in such way, that we don't need to know input and output paths beforehand
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
 pub struct Task{
     pub status: Status,
     pub time: JobTime,
-    pub command: String
+    pub command: Command
 }
 
 
 // Basic functions dealing with creation, serialization and deserialization
 impl Task{
-    pub fn new<S>(command: S) -> Self where S: Into<String>{
+    /// Create a new Task with a basic command
+    pub fn new_basic<S>(command: S) -> Self where S: Into<String>{
         Self{
             status: Status::Waiting,
             time: JobTime::new(),
-            command: command.into()
+            command: Command::new(command.into())
+        }
+    }
+
+    /// Create a new Task for a single blender frame
+    pub fn new_blender_single<S>(frame: usize, image_format: S) -> Self where S: Into<String>{
+        Self{
+            status: Status::Waiting,
+            time: JobTime::new(),
+            command: Command::new_blender_single(frame, image_format.into())
+        }
+    }
+
+    /// Create a new Task for a range of blender frames
+    pub fn new_blender_range<S>(start: usize, end: usize, step: usize, image_format: S) -> Self where S: Into<String>{
+        Self{
+            status: Status::Waiting,
+            time: JobTime::new(),
+            command: Command::new_blender_range(start, end, step, image_format.into())
         }
     }
 
@@ -52,13 +77,10 @@ impl Task{
         let deserialized: Self = serde_json::from_str(&s)?;
         Ok(deserialized)
     }
-}
 
-
-
-impl Task{
+    /// Returns true if the Tasks command is a blender Task
     pub fn is_blender(&self) -> bool{
-        self.command.starts_with("blender")
+        self.command.is_blender()
     }
 }
 
@@ -66,7 +88,8 @@ impl Task{
 
 // Methods dealing with Task.status
 impl Task{
-    /// Start the task (only if the task is waiting,)
+    /// Start the task (only if the task is waiting)
+    /// and log the time of this call
     pub fn start(&mut self){
         match self.status{
             Status::Running => (),
@@ -79,6 +102,7 @@ impl Task{
     }
 
     /// Finish the task (only if the task is running)
+    /// and log the time of this call
     pub fn finish(&mut self){
         match self.status{
             Status::Running => {
@@ -90,6 +114,7 @@ impl Task{
     }
 
     /// Error the task (only if it didn't error or finish)
+    /// and log the time of this call
     pub fn error(&mut self){
         match self.status{
             Status::Errored|Status::Finished => (),
@@ -101,6 +126,7 @@ impl Task{
     }
 
     /// Abort the task (only if it is either running, waiting or paused)
+    /// and log the time of this call
     pub fn abort(&mut self){
         match self.status{
             Status::Running|Status::Waiting|Status::Paused => {
@@ -112,6 +138,7 @@ impl Task{
     }
 
     /// Pause the task (only if it is running)
+    /// and log the time of this call
     pub fn pause(&mut self){
         match self.status{
             Status::Running => {
@@ -135,7 +162,9 @@ impl Task{
 
 
 
-/// A Tasks Status
+/// A Tasks Status describes the different states a Task can be in and allows
+/// the Task to manage all possible transitions between them. Invalid transitions
+/// are just ignored.
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
 pub enum Status{
     Waiting,
@@ -154,7 +183,7 @@ mod tests {
     use task::{Task, Status}; 
     #[test]
     fn initial_status() {
-        let t = Task::new("ls -a");
+        let t = Task::new_basic("ls -a");
         assert_eq!(t.status, Status::Waiting);
         assert_eq!(t.time.start, None);
         assert_eq!(t.time.finish, None);
@@ -163,7 +192,7 @@ mod tests {
 
     #[test]
     fn serialize_deserialze() {
-        let t1 = Task::new("ls -a");
+        let t1 = Task::new_basic("ls -a");
         match t1.serialize(){
             Ok(serialized) => {
                 if let Ok(t2) = Task::deserialize(serialized) {
@@ -176,7 +205,7 @@ mod tests {
 
     #[test]
     fn serialize_deserialze_u8() {
-        let t1 = Task::new("ls -a");
+        let t1 = Task::new_basic("ls -a");
         match t1.serialize_to_u8(){
             Ok(serialized) => {
                 if let Ok(t2) = Task::deserialize_from_u8(&serialized) {
@@ -189,9 +218,9 @@ mod tests {
 
     #[test]
     fn is_blender(){
-        let t = Task::new("blender -b");
+        let t = Task::new_blender_single(121, "PNG");
         assert_eq!(t.is_blender(), true);
-        let t = Task::new("ls -a");
+        let t = Task::new_basic("ls -a");
         assert_eq!(t.is_blender(), false);
     }
 
