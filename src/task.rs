4f17651e18dac3a8f136eacdb394094b23e8ea7e
David Huss <dh@widerstandzwecklos.at>
2018-09-27 18:26:42 +0200

    Updated documentation

--- src/task.rs
+++ src/task.rs
@@ -9,11 +9,11 @@ use ::*;
 ///
 /// This is basically a wrapper around a command, that allows us to keep track of
 /// a Tasks status, its start and end times etc. It consists of:
-/// - a [Status](enum.Status.html) which manages the States of a Task and the allowed transitions between it \
+/// - a [Status](task/enum.Status.html) which manages the States of a Task and the allowed transitions between it \
 /// (e.g. a finished task cannot be aborted, a errored task cannot start etc.)
-/// - a [JobTime](struct.JobTime.html) which allows to keep track of _when_ a certain state change has occured, as \
+/// - a [JobTime](jobtime/struct.JobTime.html) which allows to keep track of _when_ a certain state change has occured, as \
 /// well as the calculation of durations (the same construct is used for [Job](struct.Job.html))
-/// - a [Command](struct.Command.html) which allows to abstract CLI commands to be executed on the worker machines \
+/// - a [Command](command/enum.Command.html) which allows to abstract CLI commands to be executed on the worker machines \
 /// in such way, that we don't need to know input and output paths beforehand
 /// 
 /// Construct a new basic task like this:
@@ -30,9 +30,19 @@ use ::*;
 /// // Create a blender task for a range of frames (1 to 250, with a step size of 1)
 /// let mut range_frame_task = Task::new_blender_range(1, 250, 1, "PNG");
 ///
-/// // Tasks with a blender command must be constructed with paths before usage:
-/// single_frame_task.command.construct("my/blend/file.blend", "some/out/folder/####.png");
-/// range_frame_task.command.construct("my/blend/file.blend", "some/out/folder/####.png");
+/// // Tasks with a blender command must be constructed with paths before usage. 
+/// single_frame_task.construct("my/blend/file.blend", "some/out/folder/####.png");
+/// range_frame_task.construct("my/blend/file.blend", "some/out/folder/####.png");
+///
+/// // Get the constructed tasks (unwrap fails, if you didn't construct the blender tasks before):
+/// let basic_command = basic_task.to_string().unwrap();
+/// let single_frame_command = single_frame_task.to_string().unwrap();
+/// let range_frame_command = range_frame_task.to_string().unwrap();
+///
+/// // This yields following strings as a result:
+/// assert_eq!(basic_command, "ls -a".to_string());
+/// assert_eq!(single_frame_command, "blender -b --disable-autoexec my/blend/file.blend -f 121 -o some/out/folder/####.png -F PNG".to_string());
+/// assert_eq!(range_frame_command, "blender -b --disable-autoexec my/blend/file.blend -s 1 -e 250 -j 1 -o some/out/folder/####.png -F PNG".to_string());
 /// ```
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
 pub struct Task{
@@ -45,6 +55,12 @@ pub struct Task{
 // Basic functions dealing with creation, serialization and deserialization
 impl Task{
     /// Create a new Task with a basic command
+    /// ```
+    /// # extern crate bender_job;
+    /// use bender_job::Task;
+    /// // Create a new Task with the command ls -a
+    /// let t = Task::new_basic("ls -a");
+    /// ```
     pub fn new_basic<S>(command: S) -> Self where S: Into<String>{
         Self{
             status: Status::Waiting,
@@ -54,6 +70,12 @@ impl Task{
     }
 
     /// Create a new Task for a single blender frame
+    /// ```
+    /// # extern crate bender_job;
+    /// # use bender_job::Task;
+    /// // Create a new blender task that renders frame 1 as PNG
+    /// let t = Task::new_blender_single(1, "PNG");
+    /// ```
     pub fn new_blender_single<S>(frame: usize, image_format: S) -> Self where S: Into<String>{
         Self{
             status: Status::Waiting,
@@ -63,12 +85,64 @@ impl Task{
     }
 
     /// Create a new Task for a range of blender frames
+    /// ```
+    /// # extern crate bender_job;
+    /// # use bender_job::Task;
+    /// // Create a Task that renders every 10th frame from 1 to 250)
+    /// let t = Task::new_blender_range(1, 250, 10, "PNG");
+    /// ```
     pub fn new_blender_range<S>(start: usize, end: usize, step: usize, image_format: S) -> Self where S: Into<String>{
         Self{
             status: Status::Waiting,
             time: JobTime::new(),
             command: Command::new_blender_range(start, end, step, image_format.into())
         }
+    }
+
+    /// Construct a Command with the given paths. Mandatory for Blender Tasks
+    /// ```
+    /// # extern crate bender_job;
+    /// # use bender_job::Task;
+    /// // Create a Task that renders frame 121 to a PNG file
+    /// let t = Task::new_blender_single(121, "PNG");
+    ///
+    /// // Let's serialize the Task and pretend we send it to another machine:
+    /// let serialized = t.serialize_to_u8().expect("Serialization failed!");
+    /// // imagine a incredible journey through the data highway here, on the
+    /// // end of which we deserialize. t needs to be mut because we construct it later
+    /// let mut t = Task::deserialize_from_u8(&serialized).expect("Deserialization failed!");
+    ///
+    /// // Now that we are on a different machine, it could make sense to let the worker
+    /// // decide which paths to use:
+    /// let blendfile = "my/custom/file.blend";
+    /// let outpath = "render/files/here/####.png";
+    ///
+    /// // Construct the command with the new paths
+    /// t.construct(blendfile, outpath);
+    ///
+    /// // Get the string of the command (unwrap will panic if you forgot construct):
+    /// let c = t.to_string().unwrap();
+    /// assert_eq!(c, "blender -b --disable-autoexec my/custom/file.blend -f 121 -o render/files/here/####.png -F PNG".to_string());
+    /// ```
+    pub fn construct<S>(&mut self, blendfile: S, outpath: S) where S: Into<String>{
+        self.command.construct(blendfile.into(), outpath.into())
+    }
+
+    /// Convert the command to string. This returns an Error when the command is a variant \
+    /// that needs construction first (see explaination for construct method).
+    ///
+    /// ```
+    /// # extern crate bender_job;
+    /// # use bender_job::Task;
+    /// // Create a Task with the command ls -a
+    /// let t = Task::new_basic("ls -a");
+    ///
+    /// // Convert it to a string and unwrap the Result (this will never panic for a basic task)
+    /// let command = t.to_string().unwrap();
+    /// assert_eq!(command, "ls -a".to_string());
+    /// ```
+    pub fn to_string(&self) -> GenResult<String>{
+        self.command.to_string()
     }
 
     /// Serialize a Task into a String. Return a Error if this fails
@@ -84,13 +158,13 @@ impl Task{
         Ok(string.into_bytes())
     }
 
-    /// Deserialize something that fullfills Into<String> into a Task
+    /// Deserialize something that is Into<String> into a Task
     pub fn deserialize<S>(s: S) -> GenResult<Self> where S: Into<String> {
         let deserialized: Self = serde_json::from_str(&s.into()[..])?;
         Ok(deserialized)
     }
 
-    /// Deserialize something that fullfills Into<String> into a Task
+    /// Deserialize a &[u8] into a Task. This is the mirror opposite of serialize_to_u8
     pub fn deserialize_from_u8(v:&[u8]) -> GenResult<Self> {
         let s = str::from_utf8(v)?;
         let deserialized: Self = serde_json::from_str(&s)?;
@@ -98,6 +172,20 @@ impl Task{
     }
 
     /// Returns true if the Tasks command is a blender Task
+    /// Construct a Command with the given paths. Mandatory for Blender Tasks
+    /// ```
+    /// # extern crate bender_job;
+    /// # use bender_job::Task;
+    /// // Create a Task with the command ls -a
+    /// let basic_task = Task::new_basic("ls -a");
+    ///
+    /// // Create a BlenderTask that renders frame 121 as a PNG
+    /// let blender_task = Task::new_blender_single(121, "PNG");
+    /// 
+    /// // Let's check for a blend file
+    /// assert_eq!(basic_task.is_blender(), false);
+    /// assert_eq!(blender_task.is_blender(), true);
+    /// ```
     pub fn is_blender(&self) -> bool{
         self.command.is_blender()
     }
