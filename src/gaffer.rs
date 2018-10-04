189a9b3a551abf4fd7bc8106cb050ad6ff33fe3c
David Huss <dh@widerstandzwecklos.at>
2018-10-04 12:08:29 +0200

    Added additional test

--- src/gaffer.rs
+++ src/gaffer.rs
@@ -64,6 +64,10 @@ impl Gaffer for Job{
 
 
     /// Execute the checked blend-file at blend_path with the python file at python_path
+    /// The final command will look something like this:
+    /// ```text
+    /// blender -b myfile.blend --disable-autoexec --python path/to/optimize_blend.py
+    /// ```
     fn run_with_python<S>(path: S, python_path: S) -> GenResult<String>where S: Into<String>{
         let path = path.into();
         let python_path = python_path.into();
@@ -157,6 +161,21 @@ mod miscinfo{
     #[test]
     fn deserialize(){
         let data = r#"{"path":"/home/atoav/Blender/bender-job/tests/resources/data/blendfiles/jwxle4hc4xpc8go862hmxrecntw6ewnk/untitled.blend","render":{"renderer":"CYCLES","cuda":false,"device":"CPU","image_format":"PNG","uses_compositing":true},"materials":{"n":4,"removed":2},"objects":{"n":9,"removed":0},"textures":{"n":1,"removed":0},"frames":{"start":1,"end":250,"current":68,"step":1,"fps":25},"resolution":{"x":1920,"y":1080,"scale":50},"history":{"2018-09-26T09:37:55.393101+00:00":"optimize_blend.py: Sucessfully started blender with optimize_blend.py","2018-09-26T09:37:55.393124+00:00":"optimize_blend.py: Active scene.name='Scene'","2018-09-26T09:37:55.393157+00:00":"optimize_blend.py: active renderer is CYCLES","2018-09-26T09:37:55.393175+00:00":"optimize_blend.py: Found these cycles devices: <bpy_struct, CyclesDeviceSettings(\"Intel Core i7-6700K CPU @ 4.00GHz\")>","2018-09-26T09:37:55.393183+00:00":"optimize_blend.py: Error: Failed to set compute_device_type toCUDA","2018-09-26T09:37:55.440340+00:00":"optimize_blend.py: Stored changes in file at /home/atoav/Blender/bender-job/tests/resources/data/blendfiles/jwxle4hc4xpc8go862hmxrecntw6ewnk/untitled.blend"}}"#;
+        assert!(match MiscInfo::deserialize(data){
+            Ok(_info) => {
+                true
+            },
+            Err(err) => {
+                let error_message = format!("Error: failed to deserialize output to MiscInfo:\n{}\nOutput:\n{}", err, data);
+                println!("{}", error_message);
+                false
+            }
+        })
+    }
+
+    #[test]
+    fn deserialize_other(){
+        let data = r#"{"render": {"renderer": "CYCLES", "cuda": false, "device": "CPU", "image_format": "PNG", "uses_compositing": true}, "materials": {"n": 4, "removed": 4}, "objects": {"n": 18, "removed": 18}, "textures": {"n": 2, "removed": 2}, "frames": {"start": 1, "end": 500, "current": 500, "step": 1, "fps": 25}, "resolution": {"x": 1920, "y": 1080, "scale": 50}, "history": {"2018-10-04T10:03:11.110626+00:00": "optimize_blend.py: Sucessfully started blender with optimize_blend.py", "2018-10-04T10:03:11.110653+00:00": "optimize_blend.py: Active scene.name='Scene'", "2018-10-04T10:03:11.116634+00:00": "optimize_blend.py: active renderer is CYCLES", "2018-10-04T10:03:11.116677+00:00": "optimize_blend.py: Found these cycles devices: <bpy_struct, CyclesDeviceSettings(\"Intel Core i7-6700K CPU @ 4.00GHz\")>", "2018-10-04T10:03:11.116691+00:00": "optimize_blend.py: Error: Failed to set compute_device_type to CUDA", "2018-10-04T10:03:11.116741+00:00": "optimize_blend.py: Removed 4 unused Materials", "2018-10-04T10:03:11.116756+00:00": "optimize_blend.py: Removed 18 unused Objects", "2018-10-04T10:03:11.116768+00:00": "optimize_blend.py: Removed 2 unused Textures", "2018-10-04T10:03:11.218330+00:00": "optimize_blend.py: Stored changes in file at /home/atoav/Blender/bender-job/tests/resources/data/blendfiles/jjnpxq6ywd9rpasta7j93ie8yisvkyzw/a.blend"}}"#;
         assert!(match MiscInfo::deserialize(data){
             Ok(_info) => {
                 true
