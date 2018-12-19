f857df0da1e367892c34792b9ff6af16f307628d
David Huss <dh@widerstandzwecklos.at>
2018-12-19 15:19:54 +0100

    Removed install scripts in favour of global install script

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo 'Starting to install bender-job'
-echo
-
-read -e -p "
-Copy optimize_blend.py to /usr/local/lib/optimize_blend.py? [Y/n] " YN
-
-[[ $YN == "y" || $YN == "Y" || $YN == "" ]] && sudo cp ./src/optimize_blend.py /usr/local/lib/optimize_blend.py
--- uninstall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo 'Starting to remove bender-job'
-echo
-
-Do you want to remove file at /usr/local/lib/optimize_blend.py ? [Y/n] " YN
-
-    [[ $YN == "y" || $YN == "Y" || $YN == "" ]] && sudo rm -f /usr/local/lib/optimize_blend.py;
-fi
